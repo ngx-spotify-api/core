a1338b16f1988186995a8842d214518b93f6df25
Josha Landsmann <landsmann@antony-systemhaus.de>
2019-01-23 13:58:58 +0100

    removed e2e

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {NgxSpotifyApiModule} from '../../projects/ngx-spotify-api/src/lib';
+import {NgxSpotifyApiModule} from '../../projects/ngx-spotify-api-core/src/lib';
 
 @NgModule({
   declarations: [
